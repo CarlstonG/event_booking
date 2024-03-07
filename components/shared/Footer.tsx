7a2347c01b4b807505e0eae71b3031e0cf8b08fd
Carlstong <carlstongrefalde@gmail.com>
2024-03-07 22:58:03 +0800

    first commit

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -6,15 +6,16 @@ const Footer = () => {
     <footer className="border-t">
       <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
         <Link href='/'>
-          <Image 
-            src="/assets/images/logo.svg"
+          <Image className="rounded-full"
+            src="/assets/images/logo2.jpg"
             alt="logo"
-            width={128}
+            width={70}
             height={38}
           />
+         
         </Link>
-
-        <p>2023 Evently. All Rights reserved.</p>
+       
+        <p>2024 Evently. All Rights reserved.</p>
       </div>
     </footer>
   )
